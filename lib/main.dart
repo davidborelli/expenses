79ed9e1d70e892880a9a37d22e23bfa6874e6337
David Borelli <david_borelli@live.com>
2021-01-28 23:24:30 -0300

    feat: capturing form data

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,10 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
   final _transactions = [
     Transactions(
         id: 't1',
@@ -95,11 +99,13 @@ class MyHomePage extends StatelessWidget {
                 child: Column(
                   children: [
                     TextField(
+                      controller: titleController,
                       decoration: InputDecoration(
                         labelText: 'Título',
                       ),
                     ),
                     TextField(
+                      controller: valueController,
                       decoration: InputDecoration(
                         labelText: 'Valor (R\$)',
                       ),
@@ -108,14 +114,14 @@ class MyHomePage extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         TextButton(
-                          onPressed: () {}, 
-                          child: Text(
-                            'Novo Lançamento',
-                            style: TextStyle(
-                              color: Colors.purple
-                            ),
-                          )
-                        ),
+                            onPressed: () {
+                              print(titleController.text);
+                              print(valueController.text);
+                            },
+                            child: Text(
+                              'Novo Lançamento',
+                              style: TextStyle(color: Colors.purple),
+                            )),
                       ],
                     ),
                   ],
